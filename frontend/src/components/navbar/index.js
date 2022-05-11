f982aba6224f036d857a3d9a467fcc50af3875bb
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-05-11 22:42:00 +0300

    front end 2

--- frontend/src/components/navbar/index.js
+++ frontend/src/components/navbar/index.js
@@ -1,69 +1,55 @@
-
-import {Routes , Route , Link , useNavigate} from "react-router-dom"
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import Login from "../Login";
 import Register from "../Register";
 import React, { useContext } from "react";
-import { UserContext } from "../../App"
+import { UserContext } from "../../App";
+import Product from "../products";
+import GetOreders from "../Orders"
+import ProByCat from "../productByCategory"
+const Navbar = () => {
+  let { token, setToken, isLoggedIn, setIsLoggedIn } = useContext(UserContext);
 
-const Navbar =()=>{
-
-
-    let { token, setToken, isLoggedIn, setIsLoggedIn } = useContext(UserContext);
-
-
-
- 
-
-//! category= women , men , baby , boys , girls 
-
-    return <div>
-         {/* <Link to={`/products/${category}`}>
-    
-    </Link> */}
-        navbar
-
-
-        {!isLoggedIn && (
-            <Link to="/login" className="link">
-              login
-            </Link>
-          )}
-
-{!isLoggedIn && (
-            <Link to="/register" className="link">
-              register
-            </Link>
-          )}
-
-
-{/* 
-        <Link to="/women">Women</Link>
-        <Link to="/men">Men</Link>
-        <Link to="/baby">Baby</Link>
-        <Link to="boys">Boys</Link>
-        <Link to="/girls">Girls</Link> */}
-
-
-<Routes>
-{/* <Route path="/women" element={}></Route> */}
-<Route path="/login" element={<Login />} />
-<Route path="/register" element={<Register />} />
-
-</Routes>
+  //! category= women , men , baby , boys , girls
 
 
 
+
+  return (
+    <div>
+      navbar
+      {/* <Product /> */}
+      < GetOreders/>
+      {!isLoggedIn && (
+        <Link to="/login" className="link">
+          login
+        </Link>
+      )}
+      {!isLoggedIn && (
+        <Link to="/register" className="link">
+          register
+        </Link>
+      )}
+         {isLoggedIn && (
+        <Link to="/product" className="link">
+          Product
+        </Link>
+      )}
+      
+        <Link to="product/women">Women</Link>
+        <Link to="product/men">Men</Link>
+        <Link to="product/baby">Baby</Link>
+        <Link to="product/boys">Boys</Link>
+        <Link to="product/girls">Girls</Link>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        <Route path="/product" element={<Product />} />
+
+        <Route path="/product/:category" element={<ProByCat />} />
+      </Routes>
     </div>
+  );
+};
 
-
-
-
-
-
-
-
-
-
-}
-
-export default Navbar ;
+export default Navbar;
