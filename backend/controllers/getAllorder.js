0e9c60c6f8ef01054a250a483b9ed50fbeda843a
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-05-11 22:39:14 +0300

    app getAllorder

--- backend/controllers/getAllorder.js
+++ backend/controllers/getAllorder.js
@@ -2,7 +2,8 @@ const OrderSchema = require("../models/OrderSchema");
 
 const getAllorder = (req, res) => {
   const id = req.user.id;
-  OrderSchema.find({ userId: id }).populate('products.productId')
+  OrderSchema.find({ userId: id })
+  .populate('products.productId')
     
     .then((result) => {
       res.json(result);
