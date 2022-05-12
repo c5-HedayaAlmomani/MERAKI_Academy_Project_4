41a267ac43f102bfaff232eb2f27245e4cf13f7f
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-05-12 23:18:28 +0300

    edit 4

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,19 +1,27 @@
-const createNewProduct = require("../controllers/createNewProduct")
-const getAllProduct = require("../controllers/getAllProduct")
-const deleteProductById = require("../controllers/deleteProductById")
-const getProdCategory = require("../controllers/getProdCategory")
-const express = require("express")
-const isAdmin = require("../middleware/isAdmin")
-const authentication = require("../middleware/authentication")
+const createNewProduct = require("../controllers/createNewProduct");
+const getAllProduct = require("../controllers/getAllProduct");
+const deleteProductById = require("../controllers/deleteProductById");
+const getProdCategory = require("../controllers/getProdCategory");
+const express = require("express");
+const isAdmin = require("../middleware/isAdmin");
+const authentication = require("../middleware/authentication");
+
+//! =============================================
 
 const productRouter = express.Router();
 
-productRouter.post("/" ,authentication,isAdmin, createNewProduct)
-productRouter.get("/" , getAllProduct)
-productRouter.delete("/:id" , deleteProductById)
-productRouter.get("/:category" , getProdCategory)
+productRouter.post(
+  "/",
+  
+  authentication,
+  isAdmin,
+  createNewProduct
+);
+productRouter.get("/", getAllProduct);
+productRouter.delete("/:id", deleteProductById);
+productRouter.get("/:category", getProdCategory);
 
-module.exports = productRouter
+module.exports = productRouter;
 
 // createNewProduct => http://localhost:5000/product  (post)
 // getAllProduct =>  http://localhost:5000/product (get)
