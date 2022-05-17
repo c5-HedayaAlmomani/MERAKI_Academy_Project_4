e9acb0f75d026db3c6be1fe637eb303745a08128
HedayaAlmomani <almomani.hedaya@gmail.com>
2022-05-17 13:45:51 +0300

    delete order

--- backend/controllers/deleteOrderById.js
+++ backend/controllers/deleteOrderById.js
@@ -1,14 +1,30 @@
 const orderSchema = require("../models/OrderSchema");
 
+
 const deleteOrderById = (req, res) => {
   const id = req.params.id;
-
-  orderSchema.findByIdAndDelete(id, (err, result) => {
-    if (err) {
-      res.json("the id is not correct");
-    }
-
-    res.json("the order deleted");
-  });
+ 
+  console.log(req.body)
+  orderSchema
+    .findByIdAndDelete(id)
+    .then((result) => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: `The order: ${id} is not found`,
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: `order deleted`,
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        success: false,
+        message: `Server Error`,
+        err: err.message,
+      });
+    });
 };
 module.exports = deleteOrderById;
